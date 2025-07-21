947286184c18009ed9bde9fe4443b6a36c42b74d
Ananya <ananyasrivastava2407@gmail.com>
2025-07-21 20:38:05 +0530

    trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-
-
-const Page=()=>{
-
-  
+const Page=async()=>{
+  const queryClient=getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"nunya server"}));
 
   return(
-    <div >
-      Hello
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client/>
+      </Suspense>
+    </HydrationBoundary>
   );
 }
 export default Page;
